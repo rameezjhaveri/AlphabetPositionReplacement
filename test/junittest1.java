c1eeda8f7f977a77a2cab5ff4834113f0699c092
rameezjhaveri <rameez.jhaveri94@gmail.com>
2019-05-13 11:22:10 +0100

    Code development started. Code passes the first test at the moment.

--- test/junittest1.java
+++ test/junittest1.java
@@ -1,66 +1,61 @@
-import static org.junit.jupiter.api.Assertions.*;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.junit.*;
-import org.junit.jupiter.api.Test;
+package test;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-
-class junittest1 {
+public class junittest1 {
+	static String result1;
 	
-	
-	int a=1;
-	@Before
-	public void pretest() {
-		System.out.print("Starting test " + a);
+	public static String replaceLettersWithPosition(String input) {
+		return input;
 	}
 	
-
+	
 	//check to see there is a return type as string
+	@Test
 	public void test1_checkingInput() {
 		if (replaceLettersWithPosition("test input")=="test input") {
-			System.out.println("Output is String");
+			System.out.println("Test1 passed. Output is String");
 		}
 		else {
 			fail("Return type is not a string");
 		}
-		
 	}
 	
 	//compare return with correct answer to check for accuracy
 	//done multiple times to ensure answer is not correct by chance
+	@Test
 	public void test2_checkingAnswer() {
 		if (replaceLettersWithPosition("a b c abc 123.,/[")=="1 2 3 1 2 3") {
-			System.out.print("Correct Answer");
+			System.out.println("Correct Answer");
 		}
 		else {
+			System.out.println("Answer to test2 is incorrect");
 			fail("Incorrect Answer, check logic");
 		}
 	}
 	
-
-	public void test2_checkingAnswer2() {
+	@Test
+	public void test3_checkingAnswer2() {
 		if (replaceLettersWithPosition("uegclwqr0423£$^")=="21 5 7 3 12 23 17 18 ") {
-			System.out.print("Correct Answer");
+			System.out.println("Correct Answer");
 		}
 		else {
+			System.out.println("Answer to test3 is incorrect");
 			fail("Incorrect Answer, check logic");
 		}
 	}
 	
-	public void test2_checkingAnswer3() {
+	@Test
+	public void test4_checkingAnswer3() {
 		if (replaceLettersWithPosition("357659%^*£^*%")=="") {
-			System.out.print("Correct Answer");
+			System.out.println("Correct Answer");
 		}
 		else {
+			System.out.println("Answer to test4 is incorrect");
 			fail("Incorrect Answer, check logic");
 		}
 	}
-	
-	@After
-	public void posttest() {
-		System.out.print("End of test " + a);
-		a++;
-	}
-
 }
